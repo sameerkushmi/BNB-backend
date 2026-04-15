1a1ee8a9999b52dc45ccb637ffab3a40587013eb
sameer kushmi <csameer795@gmail.com>
2026-04-15 16:39:05 +0545

    penging work apr 15 04:32 PM

--- utils/cookieOptions.js
+++ utils/cookieOptions.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 const cookieOptions = {
     httpOnly: true,
     secure: isProd,
-    sameSite: isProd ? "none" : "lax",
+    sameSite: "none",
 };
 
 module.exports = cookieOptions;
